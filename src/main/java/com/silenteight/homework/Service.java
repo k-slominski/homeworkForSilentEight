07e026920f81b171974dfd9c9c9b033ace822867
Krzysztof Slominski <slominski.krzysztof0@gmail.com>
2021-04-06 23:10:31 +0200

    done: refactor repair creating mock database tried with Spring Batch but there is no point in this scenario
    todo:
    second functionality
    frontend
    handling exceptions

--- src/main/java/com/silenteight/homework/Service.java
+++ src/main/java/com/silenteight/homework/Service.java
@@ -1,150 +1,156 @@
 package com.silenteight.homework;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.*;
 
 @org.springframework.stereotype.Service
+
 public class Service
 {
-	String womenDatabaseFileName= "/women_utf8.txt";
-	String menDatabaseFileName = "/men_utf8.txt";
-	//File womenFileDatabase = new File(womenDatabaseFileName);
-	//File menFileDatabase = new File(menDatabaseFileName);
-	//InputStream in_men = getClass().getResourceAsStream(menDatabaseFileName);
-//	InputStream in_women = getClass().getResourceAsStream(womenDatabaseFileName);
+	static final String RESULT_WHEN_MALE = " MALE ";
+	static final String RESULT_WHEN_FEMALE = " FEMALE ";
+	static final String RESULT_WHEN_INCONCLUSIVE = " INCONCLUSIVE ";
+	static final String RESULT_WHEN_PROBABLY_MALE = " PROBABLY MALE ";
+	static final String RESULT_WHEN_PROBABLY_FEMALE = " PROBABLY FEMALE ";
 
-	void changeDataSource(String newMenFileDatabaseAdress,String newWomenFileDatabaseAdress)
+	static private final String WOMEN_DATABASE_FILE_NAME = "women_utf8.txt";
+	static private final String MEN_DATABASE_FILE_NAME = "men_utf8.txt";
+
+	static private File womenNamesDatabase;
+	static private File menNamesDatabase;
+
+	Service() throws Exception
 	{
-		menDatabaseFileName=newMenFileDatabaseAdress;
-		womenDatabaseFileName=newWomenFileDatabaseAdress;
-		//in_men = getClass().getResourceAsStream(newMenFileDatabaseAdress);
-		//in_women = getClass().getResourceAsStream(newWomenFileDatabaseAdress);
-	}
-	Set<String> readFile() throws IOException
-	{
-		Set <String> result = new HashSet<>();
-		//InputStream in_men = new FileInputStream(menFileDatabase);
-
-		InputStream in_men = getClass().getResourceAsStream(menDatabaseFileName);
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(in_men,StandardCharsets.UTF_8))){
-			String line;
-			while((line = reader.readLine()) != null)
-			{
-				result.add(line);
-			}
-		}
-		return result;
-
+		setDataSource();
 	}
 
-	public String guessGenderByFirstName(String name)  throws IOException
+	static void setDataSource() throws URISyntaxException
 	{
-		String[] namesArray = name.split(" ");
-		name = namesArray[0];
-		String result ="";
-//		InputStream in_men = new FileInputStream(menFileDatabase);
-//		InputStream in_women = new FileInputStream(womenFileDatabase);
+		URL resourceURLmen = Service.class.getResource("/" + MEN_DATABASE_FILE_NAME);
+		URL resourceURLwomen = Service.class.getResource("/" + WOMEN_DATABASE_FILE_NAME);
 
-		InputStream in_men = getClass().getResourceAsStream(menDatabaseFileName);
-		InputStream in_women = getClass().getResourceAsStream(womenDatabaseFileName);
+		menNamesDatabase = Paths.get(resourceURLmen.toURI()).toFile();
+		womenNamesDatabase = Paths.get(resourceURLwomen.toURI()).toFile();
+	}
 
-		if (in_men == null) System.out.println("null");
+	static void changeDataSource(File newMenNamesDatabase, File newWomenNamesDatabase)
+	{
+		menNamesDatabase = newMenNamesDatabase;
+		womenNamesDatabase = newWomenNamesDatabase;
+	}
 
+	static public String guessGenderByFirstName(String name) throws Exception
+	{
+		name = name.substring(0, name.indexOf(" "));
+		String result = "";
 
-		try (BufferedReader readerMen = new BufferedReader(new InputStreamReader(in_men, StandardCharsets.UTF_8));
-			 BufferedReader readerWomen = new BufferedReader(new InputStreamReader(in_women, StandardCharsets.UTF_8)))
-		{
+		try (BufferedReader readerMen = new BufferedReader(new FileReader(menNamesDatabase));
+			 BufferedReader readerWomen = new BufferedReader(new FileReader(womenNamesDatabase))) {
+
 			String menLine = readerMen.readLine().replaceFirst("^\uFEFF", "");
 			String womenLine = readerWomen.readLine().replaceFirst("^\uFEFF", "");
-			result = name.equalsIgnoreCase(menLine) ? " MALE " : name.equalsIgnoreCase(womenLine) ? " FEMALE " : " INCONCLUSIVE ";
 
-			while ((menLine = readerMen.readLine()) != null) {
+			result = name.equalsIgnoreCase(menLine) ? RESULT_WHEN_MALE : name.equalsIgnoreCase(womenLine) ?
+					RESULT_WHEN_FEMALE : RESULT_WHEN_INCONCLUSIVE;
+
+			while ((menLine = readerMen.readLine()) != null && !result.equalsIgnoreCase(RESULT_WHEN_MALE)) {
 				if (name.equalsIgnoreCase(menLine)) {
-					result = " MALE ";
+					result = RESULT_WHEN_MALE;
 					break;
 				}
 			}
-			while ((womenLine = readerWomen.readLine()) != null) {
+			while ((womenLine = readerWomen.readLine()) != null && !result.equalsIgnoreCase(RESULT_WHEN_FEMALE)) {
 				if (name.equalsIgnoreCase(womenLine)) {
-					result = " FEMALE ";
+					result = RESULT_WHEN_FEMALE;
 					break;
 				}
 			}
 
-		}catch(NullPointerException nle)
-		{
+		} catch(NullPointerException nle) {
 			nle.printStackTrace();
 		}
 		return result;
 	}
 
-	String guessGenderByRuleOfMajority(String name)  throws IOException
+	static String guessGenderByRuleOfMajority(String name) throws Exception
 	{
 		List<String> namesList = new LinkedList<>(Arrays.asList(name.split(" ")));
 		ListIterator<String> iterator = namesList.listIterator();
+
+		String temp;
+		String inCaseOfDraw = RESULT_WHEN_INCONCLUSIVE;
+		String firstName = namesList.get(0);
+
 		int menCounter = 0;
 		int womenCounter = 0;
-		String temp = "";
-		String inCaseOfDraw = " INCONCLUSIVE ";
-		String firstName = namesList.get(0);
-		InputStream in_men = getClass().getResourceAsStream(menDatabaseFileName);
-		InputStream in_women = getClass().getResourceAsStream(womenDatabaseFileName);
-		try (BufferedReader readerMen = new BufferedReader(new InputStreamReader(in_men, StandardCharsets.UTF_8));
-			 BufferedReader readerWomen = new BufferedReader(new InputStreamReader(in_women, StandardCharsets.UTF_8))) {
+
+		try (BufferedReader readerMen = new BufferedReader(new FileReader(menNamesDatabase));
+			 BufferedReader readerWomen = new BufferedReader(new FileReader(womenNamesDatabase))) {
+
 			String menLine = readerMen.readLine().replaceFirst("^\uFEFF", "");
 			String womenLine = readerWomen.readLine().replaceFirst("^\uFEFF", "");
 
-			while(iterator.hasNext()) {
-				temp=iterator.next();
+			while (iterator.hasNext()) {
+				temp = iterator.next();
 				if (temp.equalsIgnoreCase(menLine)) {
-					inCaseOfDraw = firstName.equalsIgnoreCase(temp)?" PROBABLY MALE ": inCaseOfDraw;
+					inCaseOfDraw = firstName.equalsIgnoreCase(temp) ? RESULT_WHEN_PROBABLY_MALE : inCaseOfDraw;
 					menCounter++;
 					namesList.remove(temp);
 					iterator = namesList.listIterator();
-				}
-				else {
+				} else {
 					if (temp.equalsIgnoreCase(womenLine)) {
-						inCaseOfDraw = firstName.equalsIgnoreCase(temp)?" PROBABLY FEMALE ": inCaseOfDraw;
+						inCaseOfDraw = firstName.equalsIgnoreCase(temp) ? RESULT_WHEN_FEMALE : inCaseOfDraw;
 						womenCounter++;
 						namesList.remove(temp);
 						iterator = namesList.listIterator();
 					}
 				}
 			}
-			System.out.println(" first " + menCounter +" "+ womenCounter + " ");
-
-
-			while ((menLine = readerMen.readLine()) != null && namesList != null) {
+			while ((menLine = readerMen.readLine()) != null) {
 				iterator = namesList.listIterator();
-				while(iterator.hasNext()) {
+				while (iterator.hasNext()) {
 					temp = iterator.next();
 					if (temp.equalsIgnoreCase(menLine)) {
 						menCounter++;
 						namesList.remove(temp);
 						iterator = namesList.listIterator();
-						inCaseOfDraw = firstName.equalsIgnoreCase(temp)?" PROBABLY MALE ": inCaseOfDraw;
-
+						inCaseOfDraw = firstName.equalsIgnoreCase(temp) ? RESULT_WHEN_PROBABLY_MALE : inCaseOfDraw;
 					}
 				}
 			}
-
-			while ((womenLine = readerWomen.readLine()) != null && namesList != null) {
+			while ((womenLine = readerWomen.readLine()) != null) {
 				iterator = namesList.listIterator();
-				while(iterator.hasNext()) {
-					temp=iterator.next();
+				while (iterator.hasNext()) {
+					temp = iterator.next();
 					if (temp.equalsIgnoreCase(womenLine)) {
 						womenCounter++;
 						namesList.remove(temp);
 						iterator = namesList.listIterator();
-						inCaseOfDraw = firstName.equalsIgnoreCase(temp)?" PROBABLY FEMALE ": inCaseOfDraw;
+						inCaseOfDraw = firstName.equalsIgnoreCase(temp) ? RESULT_WHEN_PROBABLY_FEMALE : inCaseOfDraw;
 					}
 				}
 			}
 		}
-		System.out.println(" last "+ menCounter +" "+ womenCounter);
 
-		return menCounter > womenCounter ? " MALE " : menCounter < womenCounter ? " FEMALE " : inCaseOfDraw;
+		return menCounter > womenCounter ? RESULT_WHEN_MALE : menCounter < womenCounter ? RESULT_WHEN_FEMALE :
+				inCaseOfDraw;
+	}
+
+	Set<String> readFile() throws Exception
+	{
+		Set<String> result = new HashSet<>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(menNamesDatabase))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				result.add(line);
+			}
+		}
+		return result;
 
 	}
 }
--- src/main/java/com/silenteight/homework/Servlet.java
+++ src/main/java/com/silenteight/homework/Servlet.java
@@ -1,14 +1,11 @@
 package com.silenteight.homework;
 
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
-import java.nio.channels.ScatteringByteChannel;
 import java.util.Set;
 
 @RestController
@@ -17,32 +14,32 @@ public class Servlet
 {
 	private final Service service;
 
-	Servlet (Service service)
+	Servlet(Service service)
 	{
-		this.service=service;
+		this.service = service;
 	}
 
 	@ResponseBody
 	@RequestMapping("/1")
-	public Set<String> writeContentOfTheFile() throws IOException
+	public Set<String> writeContentOfTheFile() throws IOException, Exception
 	{
 
-		return  service.readFile();
+		return service.readFile();
 	}
 
 	@RequestMapping("/2")
 	@ResponseBody
-	public String guessingGenderByFirstName (@RequestParam (value = "name") String name) throws IOException
+	public String guessingGenderByFirstName(@RequestParam(value = "name") String name) throws Exception
 	{
-		return "Gender: "+ service.guessGenderByFirstName(name);
+		return "Gender: " + service.guessGenderByFirstName(name);
 
 	}
 
 	@RequestMapping("/3")
 	@ResponseBody
-	public String guessingGenderByRuleOfMajority (@RequestParam (value = "name") String name) throws IOException
+	public String guessingGenderByRuleOfMajority(@RequestParam(value = "name") String name) throws Exception
 	{
-		return "Gender: "+ service.guessGenderByRuleOfMajority(name);
+		return "Gender: " + service.guessGenderByRuleOfMajority(name);
 
 	}
 
